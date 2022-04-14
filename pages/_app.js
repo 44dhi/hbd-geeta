dace1f266fe7b5920a150cdfda627db8fc1e6ca7
Aadhit <70216072+Aadhit@users.noreply.github.com>
2022-04-14 20:44:26 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Head>
+    <title>Happy Birthday Geeta Girumala 🥳 </title>
+    <link rel="icon" type="image/x-icon" href="/fav.svg" />
+  </Head>
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
