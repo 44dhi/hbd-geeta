974f1f0bcf49da20ebf6d00e299e66354e757bb6
Aadhit <70216072+Aadhit@users.noreply.github.com>
2022-04-15 10:01:55 +0530

    If geeta click link

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
+/* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable jsx-a11y/alt-text */
+import { motion } from 'framer-motion'
 import Head from 'next/head'
 import Image from 'next/image'
 import React,{useState,useEffect} from 'react'
@@ -7,6 +9,18 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
 
   const [whichImage,setWhichImage] = useState(false);
+
+  const animations = {
+    hidden:{
+        opacity:0,
+    },
+    visible:{
+        opacity:1,
+        transition:{
+            duration:.8,
+        }
+    },
+  }
 
   useEffect(()=>{
 
@@ -54,6 +68,9 @@ export default function Home() {
           </div>
         </div>
       }
+      <motion.div initial="hidden" animate="visible" variants={animations} className={styles.textContainer}>
+        <span>If your name is Geeta, click this <a target="_blank" href='https://peppered-piccolo-e47.notion.site/Geeta-Birthday-4289770353fd4b19b379feaf96d8f795'>link</a></span>
+      </motion.div>
     </div>
   )
 }
